e38ffca2aeaf1a6327a41748f3db103590d81007
itsme-boii <tushark.tec@gmail.com>
2024-01-13 20:06:38 +0530

    api

--- server.js
+++ server.js
@@ -1,154 +1,132 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const app = express();
-// const port = process.env.PORT || 3000;
 
-// app.use(cors());
-// app.use(express.json());
-
-// let gameScores = [];
-// let sfIds = [];
-// let userScores = {};
-
-
-// app.post('/api/score', (req, res) => {
-//     const { newScore ,sfId} = req.body;
-//     if (!sfId) {
-//         return res.status(400).json({ success: false, message: 'Authentication token is required' });
-//     }
-//     console.log('Received Score Data:', req.body);
-//     console.log(newScore);
-//     if (!userScores[sfId]) {
-//         userScores[sfId] = [];
-//     }
-
-//     userScores[authToken].push(newScore);
-
-//     res.json({ success: true, message: 'Score is Updated', scores: userScores[sfId] });
-// });
-
-// app.get('/api/score', (req, res) => {
-//     res.json({ score: gameScores });
-// });
-
-
-// // Route to post sfId
-// app.post('/api/sfId', (req, res) => {
-//     const { sfId } = req.body;
-
-//     if (sfId !== undefined) {
-//         sfIds.push(sfId);
-//         res.json({ success: true, message: 'sfId is Updated', sfIds });
-//     } else {
-//         res.status(400).json({ success: false, message: 'Invalid request format' });
-//     }
-// });
-
-// // Route to get sfIds
-// app.get('/api/sfId', (req, res) => {
-//     res.json({ sfIds });
-// });
-
-// app.post('/api/index', (req, res) => {
-//     const { index } = req.body;
-
-//     if (index !== undefined) {
-//         global.index = index;
-//         res.json({ success: true, message: 'index is Updated', index: global.index });
-//     } else {
-//         res.status(400).json({ success: false, message: 'Invalid request format' });
-//     }
-// });
-
-// app.get('/api/index', (req, res) => {
-//     res.json( {index} );
-// });
-
-
-
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mysql = require('mysql2');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 
-let userScores = {};
-let sfIds = [];
-let sfIdMapped={};
-let authToken = [];
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'manyag81@81',
+    database: 'gamedb',
+});
 
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL');
+});
+
+let userScores = {};
+let sfIdMapped = {};
+let sfid2maxscore = {};
 
 //this is used to update score
 //input score and authtoken 
 app.post('/api/score', (req, res) => {
-    const { newScore, authToken } = req.body;
-    
-    if (!authToken) {
-        return res.status(400).json({ success: false, message: 'Authentication token is required' });
+
+    try {
+        const { newScore, authtoken } = req.body;
+
+        if (!authtoken) {
+            return res.status(400).json({ success: false, message: 'Authentication token is required' });
+        }
+
+        console.log('Received Score Data:', req.body);
+        console.log(newScore);
+
+        if (!userScores[authtoken]) {
+            userScores[authtoken] = [];
+        }
+
+        userScores[authtoken].push(newScore);
+
+        res.json({ success: true, message: 'Score is Updated', scores: userScores });
     }
-
-    console.log('Received Score Data:', req.body);
-    console.log(newScore);
-
-    if (!userScores[authToken]) {
-        userScores[authToken] = [];
+    catch (err) {
+        return err;
     }
-
-    userScores[authToken].push(newScore);
-
-    res.json({ success: true, message: 'Score is Updated', scores: userScores[authToken] });
 });
 
 
 // this will send all scores from backend to frontend
 app.get('/api/score', (req, res) => {
-    res.json({ scores: userScores });
+    try {
+        res.json({ scores: userScores, sfidMappedtoauthtoken: sfIdMapped, maxscore: sfid2maxscore });
+    }
+    catch (err) {
+        return err;
+    }
 });
 
 
 // this will map my sf id with my authentication token and also this contain array of sfIds
 //inputs are sfid and authtoken
-app.post('/api/sfId', (req, res) => {
-    const { sfId, authToken } = req.body;
+app.post('/api/sfId2authtoken', (req, res) => {
+    try {
+        const { sfId, authToken } = req.body;
 
-    if (!authToken || sfIds.includes(authToken)) {
-        return res.status(400).json({ success: false, message: 'Invalid request format or duplicate authToken' });
+        if (!authToken) {
+            return res.status(400).json({ success: false, message: 'Invalid request format or duplicate authToken' });
+        }
+
+        sfIdMapped[sfId] = authToken;
+        userScores[authToken] = [];
+
+        res.json({ success: true, message: 'sfId is Updated', sfIdMapped });
     }
-
-    sfIds.push(sfId);
-    sfIdMapped[authToken]=sfId;
-    userScores[authToken] = [];
-
-    res.json({ success: true, message: 'sfId is Updated', sfIds,sfIdMapped });
+    catch (err) {
+        return err;
+    }
 });
 
 
 //will get mapped sfids from backend mapping authtoken and my sfid
 //will be used by front end
-app.get('/api/sfId', (req, res) => {
-    res.json({ sfIdMapped });
+app.get('/api/sfId2authtoken', (req, res) => {
+    try {
+        res.json({ sfIdMapped });
+    }
+    catch (err) {
+        return err;
+    }
 });
 
+//sfid2score
+app.post('/api/sfid2score', (req, res) => {
+    try {
+        const { sfId, maxscore } = req.body;
+        if (!sfId) {
+            return res.status(400).json({ success: false, message: 'Invalid sfId' });
+        }
+        sfid2maxscore[sfId] = maxscore;
+        const insertQuery = `
+            INSERT INTO sfid2score (sfId, maxscore) VALUES (?, ?)
+            ON DUPLICATE KEY UPDATE maxscore = ?;
+        `;
 
-//used by game to post authtoken
-app.post('/api/authtoken',(req,res)=>{
-    const { authtoken } = req.body;
-    authToken.push(authtoken);
-    res.json({ success: true, message: 'authtoken updated', authToken });
-})
-
-//called by front end to get the authtoken
-app.get('/api/authtoken',(req,res)=>{
-    res.json({authToken});
-})
+        db.query(insertQuery, [sfId, maxscore, maxscore], (err, result) => {
+            if (err) {
+                console.error('Error inserting sfid2score into the database:', err);
+                res.status(500).json({ success: false, message: 'Internal Server Error' });
+            } else {
+                console.log('sfid2score inserted or updated successfully');
+                res.json({ success: true, message: 'sfId mapped to max score in the database', sfid2maxscore });
+            }
+        });
+    }
+    catch (err) {
+        return err;
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
+})
